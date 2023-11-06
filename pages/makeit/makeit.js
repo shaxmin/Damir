c18c567a317d17f8da9499011dd8370549a2fe21
shaxmin <stalkerktl@gmail.com>
2023-11-06 14:04:28 +0600

    add makeit page

--- pages/makeit/makeit.js
+++ pages/makeit/makeit.js
@@ -1,6 +1,7 @@
 "use strict"
 
 const experienceDiv = document.querySelectorAll('.infoDiv');
+const links = document.querySelectorAll('.toTop');
 
 experienceDiv.forEach(item => {
     let key = item.dataset.key;
@@ -12,4 +13,17 @@ experienceDiv.forEach(item => {
                         ${data[key].rightList}
                 </div>
     `
+});
+
+links.forEach(item => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault()
+        let link = e.target.getAttribute('href')
+        const elem = document.querySelector(link)
+        let top = elem.getBoundingClientRect().top
+        window.scrollTo({
+        top: top + window.scrollY - 55,
+        behavior: 'smooth'
+    })
+})
 });
