0e859ee63459c558a4afb44740e90f76d8e5939e
shaxmin <stalkerktl@gmail.com>
2023-11-07 12:50:40 +0600

    add: global page

---
+++ pages/global/global.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const experienceDiv = document.querySelectorAll(".infoDiv");
+const links = document.querySelectorAll('.toTop');
+
+experienceDiv.forEach((item) => {
+  let key = item.dataset.key;
+  item.innerHTML = `
+            <div class="leftSide">
+                <div class="leftTitle">${data[key].leftTitle}</div>
+            </div>
+            <div class="right">
+                        ${data[key].rightList}
+                </div>
+    `;
+});
+
+links.forEach(item => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault()
+        let link = e.target.getAttribute('href')
+        const elem = document.querySelector(link)
+        let top = elem.getBoundingClientRect().top
+        window.scrollTo({
+        top: top + window.scrollY - 55,
+        behavior: 'smooth'
+    })
+})
+});
